22aa3e6c4d52402f559e0ca373842ec7cebe024a
ShirleyLu2014 <1090165836@qq.com>
2022-09-09 10:58:13 +0800

    haha

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,5 @@ import App from "./App.vue"
 import { globalRegister } from "./global"
 const app = createApp(App)
 app.use(globalRegister)
+
 app.mount("#app")
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import { join } from "path"
+import eslintPlugin from "vite-plugin-eslint"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    eslintPlugin({
+      include: ["src/**/*.js", "src/**/*.vue", "src/*.js", "src/*.vue"]
+    })
+  ],
   resolve: {
     alias: {
       "@": join(__dirname, "/src")
